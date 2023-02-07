b722e3ceabb9491c74fe8bf3e655cf591e5f6391
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-02-07 10:11:41 +0900

    재생시 메세지 순차적으로 렌더링, 음성 setInterval()로 일단은 완성

--- src/components/main/View.jsx
+++ src/components/main/View.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "../../css/View.module.css";
 import { BsPlayCircleFill } from "react-icons/bs";
 import { MessageContext } from "../../context/MessageContext";
@@ -7,14 +7,22 @@ import Talk from "./talk/Talk";
 import { PlayStateContext } from "../../context/PlayStateContext";
 
 export default function View() {
-  const { messages } = useContext(MessageContext);
+  const { messages, messages_to_play, fillMessages_to_play } =
+    useContext(MessageContext);
   const { speak } = useContext(SSTContext);
   const { setPlayState } = useContext(PlayStateContext);
+
   const handleClick = (e) => {
-    messages.forEach((message) => {
-      speak(message.text);
-    });
     setPlayState(true);
+
+    let count = 0;
+    let interval = setInterval(() => {
+      fillMessages_to_play(messages[count]);
+      speak(messages[count].text);
+      count++;
+
+      if (count === messages.length) clearInterval(interval);
+    }, 2000);
   };
 
   return (
--- src/context/MessageContext.jsx
+++ src/context/MessageContext.jsx
@@ -6,12 +6,28 @@ export function MessageProvider({ children }) {
   const [messages, setMessages] = useState([
     { text: "안녕", owner: "my-message", image: "" },
     { text: "안녕하신가", owner: "other-message", image: "" },
-    { text: "안녕하신가", owner: "other-message", image: "" },
     { text: "쿠쿠루삥뽕", owner: "my-message", image: "" },
   ]);
 
+  const [messages_to_play, setMessages_to_play] = useState("");
+
+  const fillMessages_to_play = (message) => {
+    /**
+     * 메세지를 반복하면서 재생할메세지를 채워
+     */
+    setMessages_to_play((prev) => [...prev, message]);
+  };
+
   return (
-    <MessageContext.Provider value={{ messages, setMessages }}>
+    <MessageContext.Provider
+      value={{
+        messages,
+        setMessages,
+        messages_to_play,
+        setMessages_to_play,
+        fillMessages_to_play,
+      }}
+    >
       {children}
     </MessageContext.Provider>
   );
