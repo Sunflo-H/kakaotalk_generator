c66444e6d7282644e39bd918f97908fafbcb6793
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-02-10 08:46:58 +0900

    토크 생성기 이미지 추가

--- src/components/main/TalkGenerator.jsx
+++ src/components/main/TalkGenerator.jsx
@@ -1,8 +1,8 @@
 import React, { useContext, useState } from "react";
 import styles from "../../css/TalkGenerator.module.css";
-import { BsCardImage } from "react-icons/bs";
+import { BiMessageRoundedAdd } from "react-icons/bi";
+import { MdAddPhotoAlternate } from "react-icons/md";
 import { MessageContext } from "../../context/MessageContext";
-import { SSTContext } from "../../context/SSTContext";
 
 export default function TalkGenerator() {
   const { messages, setMessages } = useContext(MessageContext);
@@ -34,45 +34,58 @@ export default function TalkGenerator() {
     ]);
   };
 
-  // 내 기능
-  // 메세지 입력, 사진 추가
-
-  // 상대방 기능
-  // 상대방 추가(이름, 프로필), 메세지 입력, 사진 추가
-
-  // 일단 메세지만 만들어보자
   return (
     <div className={styles["talk-generators"]}>
       <div className={styles.generator}>
         <div className={styles["header"]}>My Talk</div>
+
         <form className={styles.main} onSubmit={handleSubmit_mine}>
           <label htmlFor="my_message">Message </label>
-          <input
-            type="text"
-            id="my_message"
-            placeholder="메세지 입력"
-            onChange={handleChange_input_mine}
-            value={input_mine}
-          />
-          <button>추가</button>
-          {/* 사진추가 */}
-          {/* <BsCardImage className={styles.icons} /> */}
+          <div className={styles.flex}>
+            <input
+              type="text"
+              id="my_message"
+              placeholder="메세지 입력"
+              onChange={handleChange_input_mine}
+              value={input_mine}
+            />
+            <button className={styles.add}>
+              <BiMessageRoundedAdd className={styles.icons} />
+            </button>
+          </div>
+
+          <label htmlFor="my_image">Image</label>
+          <div className={styles.flex}>
+            <input type="file" id="my_image" />
+            <button className={styles.add}>
+              <MdAddPhotoAlternate className={styles.icons} />
+            </button>
+          </div>
         </form>
       </div>
       <div className={styles.generator}>
         <div className={styles["header"]}>Other Talk</div>
         <form className={styles.main} onSubmit={handleSubmit_other}>
           <label htmlFor="other_message">Message </label>
-          <input
-            type="text"
-            id="other_message"
-            placeholder="메세지 입력"
-            onChange={handleChange_input_other}
-            value={input_other}
-          />
-          <button>추가</button>
-          {/* 사진추가 */}
-          {/* <BsCardImage className={styles.icons} /> */}
+          <div className={styles.flex}>
+            <input
+              type="text"
+              id="other_message"
+              placeholder="메세지 입력"
+              onChange={handleChange_input_other}
+              value={input_other}
+            />
+            <button className={styles.add}>
+              <BiMessageRoundedAdd className={styles.icons} />
+            </button>
+          </div>
+          <label htmlFor="other_image">Image</label>
+          <div className={styles.flex}>
+            <input type="file" id="other_image" />
+            <button className={styles.add}>
+              <MdAddPhotoAlternate className={styles.icons} />
+            </button>
+          </div>
         </form>
       </div>
     </div>
